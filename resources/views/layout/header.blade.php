bcfe8754494769c146d826b64e8179aae0cd3c9e
Alessio Spallino <alessiospallino@dhcp-86-145.eduroam.aalto.fi>
2017-11-05 15:38:51 +0200

    New header when logged in + img

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -19,3 +19,43 @@
 		</div>
 	</div>
 </header>
+
+<!-- LOG IN HEADER 
+<header>
+	<div class="container col-md-offset-1 col-md-10 header">
+		<div class="row col-md-12 align" >	
+			<div class="col-md-2 logo">
+				 AmuseMie 
+			</div>
+			<div class="col-md-1 eventMenu menu">
+				 <a href="#" class="text selectedMenu"> EVENTS </a>
+			</div>
+			<div class="col-md-2 menu">
+				 <a href="#" class="text"> ASSOCIATION </a>
+			</div>
+			<div class="col-md-2 menu">
+				 <a href="#" class="text"> MY DASHBOARD</a>
+			</div>
+			
+			<div class="dropdown col-md-offset-2  col-md-3 menu">
+				<button class="dropdown-toggle col-md-12 raleway hiddenDropDown" type="button" id="dropdownMenuLogIn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+				<span id="name">Chuck Norris </span>
+				<img class="logPic" src="{{ asset('img/chuckNorris.png') }}">
+				<img style="width:20px" src="{{ asset('img/dot.png') }}">
+			  </button>
+			  <ul class="dropdown-menu col-md-12" aria-labelledby="dropdownMenu1">
+				<li><a href="#">PROFILE</a></li>
+				 <li class="divider"></li> 
+				<li><a href="#">LOG OUT</a></li>
+				 <li class="divider"></li> 
+				<li><a href="#">ASSOCIATION 1</a></li>
+				<li class="divider"></li> 
+				<li><a href="#">ASSOCIATION 2</a></li>
+				<li class="divider"></li> 
+				<li><a href="#">CREATE NEW ASSOCIATION</a></li>
+			  </ul>
+			</div>
+		</div>
+	</div>
+</header>
+-->
