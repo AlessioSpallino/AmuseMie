53f0a96e0b972d853c8b18bc8ec09247b70496ef
Matrout96 <martamoyag26@gmail.com>
2017-11-08 13:58:02 +0200

    Login

---
+++ resources/views/logIn.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layout.head');
+<body>
+<div class="container raleway" style="text-align:center">
+    <h1 style="padding-bottom:20px"> LOGIN WITH</h1>
+
+    <div>
+        <button class="button-facebook"><img class="pequeña" src="../../public/img/facebook-logo-blanco.png" style="width: 10%">  Login with Facebook</button>
+    </div>
+    <div style="margin-top: 16px">
+        <button class="button-google"><img class="pequeña" src="../../public/img/google-plus.png" style="width: 10%">  Login with Google</button>
+    </div>
+    <div style="margin-top: 16px">
+        <button class="button-aalto"><img class="pequeña" src="../../public/img/Aalto-logo.png" style="width: 17%">  Login with Aalto</button>
+    </div>
+
+</div>
+
+</body>
+
+</html>
