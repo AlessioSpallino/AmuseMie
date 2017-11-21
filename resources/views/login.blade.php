6694ef5a40603fdbe0fc1413756ebdcccddf60ae
Alessio Spallino <alessiospallino@Alessios-MacBook-Pro.local>
2017-11-21 13:28:36 +0200

    Login page fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('layout.head');
+@include('layout.head')
 <body>
 <div class="container raleway" style="text-align:center">
     <h1 style="padding-bottom:20px">LOGIN WITH</h1>
@@ -11,8 +11,7 @@
             <li><a href="{{ url('login/facebook') }}"><button class="btn btn-facebook"><i class="fa fa-facebook pull-left" aria-hidden="true"></i>Sign in with Facebook</button></a></li>
             
             <li><a href="{{ url('login/google') }}"><button class="btn btn-google"><i class="fa fa-google-plus pull-left" aria-hidden="true"></i>Sign in with Google</button></a></li>
-            
-            <li><button class="btn btn-aalto"><i class="fa fa-pinterest-p pull-left" aria-hidden="true"></i>Sign in with Aalto</button></li>
+
         </ul>
     </div>
 
